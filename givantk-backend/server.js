6fd2c3c33851c204217dbad21e28422f4988bf0f
Abdo <abdopoetry@gmail.com>
2018-10-23 17:42:15 +0200

    Added a new Mlab account and link

--- givantk-backend/server.js
+++ givantk-backend/server.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
+const express = require("express");
+const mongoose = require("mongoose");
 
 //including users routes
-const users_routes = require('./api/routes/users');
+const users_routes = require("./api/routes/users");
 //including services routes
-const services_routes = require('./api/routes/services'); 
+const services_routes = require("./api/routes/services");
 
 const app = express();
 
@@ -13,16 +13,19 @@ const app = express();
 app.use(bodyParser.json());
 
 /* START CONNECTION TO MONGODB */
-mongoose.connect('mongodb://osama:osama123@ds137483.mlab.com:37483/givantk', {useNewUrlParser: true})
-        .then( () => console.log("MongoDB Connected"))
-        .catch( err => console.log(err) );
+mongoose
+  .connect(
+    "mongodb://givantk:givantk123@ds149481.mlab.com:49481/givantk",
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
 /* END CONNECTION TO MONGODB */
 
-
 // use users routes
-app.use('/api/users', users_routes);
+app.use("/api/users", users_routes);
 // use services routes
-app.use('/api/services', services_routes);
+app.use("/api/services", services_routes);
 
 const port = process.env.PORT || 3000;
 
