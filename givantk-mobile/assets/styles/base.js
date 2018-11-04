90e803db91a4acf7c896885e21c17355da2d1ea0
Abdo <abdopoetry@gmail.com>
2018-11-04 16:06:51 +0200

    Added base css configurations

---
+++ givantk-mobile/assets/styles/base.js
@@ -0,0 +1,29 @@
+import { Dimensions } from "react-native";
+
+export const dimensions = {
+  fullHeight: Dimensions.get("window").height,
+  fullWidth: Dimensions.get("window").width
+};
+
+export const colors = {
+  primary: "#C94B28",
+  secondary: "#2B6BA0",
+  tertiary: "#9E711F",
+  black: "#232323",
+  white: "#EAEEFF"
+};
+
+export const padding = {
+  sm: 10,
+  md: 20,
+  lg: 30,
+  xl: 40
+};
+
+export const fontSizes = {
+  sm: 12,
+  md: 18,
+  lg: 28
+};
+
+//main colors:https://coolors.co/c94b28-2b6ba0-9e711f-232323-eaeeff
