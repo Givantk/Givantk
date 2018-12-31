41944614eeee1e59d90b148215980b92a0a6f3c0
Abdo <abdopoetry@gmail.com>
2018-12-31 11:59:20 +0200

    Improved the flatlist render performance

--- givantk-mobile/src/components/commons/Service-Related-Components/ServiceCard/ServiceCard.js
+++ givantk-mobile/src/components/commons/Service-Related-Components/ServiceCard/ServiceCard.js
@@ -5,40 +5,42 @@ import React from 'react';
 
 import styles from './ServiceCardStyles';
 
-const ServiceCard = (props) => {
-  const { service, navigateToServiceScreen } = props;
+class ServiceCard extends React.PureComponent {
+  render() {
+    const { service, navigateToServiceScreen } = this.props;
 
-  return (
-    <TouchableWithoutFeedback onPress={navigateToServiceScreen}>
-      <View style={styles.serviceCard}>
-        <View style={styles.header}>
-          <Image
-            source={{
-              uri: service.poster.imageURL,
-            }}
-            style={styles.userImage}
-          />
-          <View style={styles.headerRight}>
-            <Text style={styles.userName}>{service.poster.name}</Text>
-            <Text style={styles.serviceTitle}>{service.title}</Text>
+    return (
+      <TouchableWithoutFeedback onPress={navigateToServiceScreen}>
+        <View style={styles.serviceCard}>
+          <View style={styles.header}>
+            <Image
+              source={{
+                uri: service.poster.imageURL,
+              }}
+              style={styles.userImage}
+            />
+            <View style={styles.headerRight}>
+              <Text style={styles.userName}>{service.poster.name}</Text>
+              <Text style={styles.serviceTitle}>{service.title}</Text>
+            </View>
+          </View>
+
+          <View style={styles.content}>
+            <Text style={styles.descriptionText}>{service.description}</Text>
+          </View>
+
+          <View style={styles.footer}>
+            <Text style={styles.cost}>{service.cost}</Text>
+            <View style={styles.footerLeft}>
+              <Icon type="EvilIcons" name="envelope" style={styles.shareIcon} />
+              <Icon type="Feather" name="star" style={styles.favoriteIcon} />
+            </View>
           </View>
         </View>
-
-        <View style={styles.content}>
-          <Text style={styles.descriptionText}>{service.description}</Text>
-        </View>
-
-        <View style={styles.footer}>
-          <Text style={styles.cost}>{service.cost}</Text>
-          <View style={styles.footerLeft}>
-            <Icon type="EvilIcons" name="envelope" style={styles.shareIcon} />
-            <Icon type="Feather" name="star" style={styles.favoriteIcon} />
-          </View>
-        </View>
-      </View>
-    </TouchableWithoutFeedback>
-  );
-};
+      </TouchableWithoutFeedback>
+    );
+  }
+}
 
 export default ServiceCard;
 
