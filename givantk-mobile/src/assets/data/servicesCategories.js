436561ad5eeeafd679ef03b9d43053c57ab9b3d5
Abdo <abdopoetry@gmail.com>
2019-01-28 21:36:57 +0200

    Update service categories in the front end

--- givantk-mobile/src/assets/data/servicesCategories.js
+++ givantk-mobile/src/assets/data/servicesCategories.js
@@ -1,3 +1,8 @@
-const servicesCategories = ['A', 'B', 'C', 'D'];
+const servicesCategories = [
+  'Knowledge Exchange',
+  'Everyday Services',
+  'Reach the community',
+  'Others',
+];
 
 export default servicesCategories;
