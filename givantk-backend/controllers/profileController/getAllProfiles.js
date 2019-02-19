0e4242627fb252612e925a1f2f2bcf96a2cd29d1
Abdo <abdopoetry@gmail.com>
2019-02-19 20:54:12 +0200

    Populate services in getAllProfiles controller

--- givantk-backend/controllers/profileController/getAllProfiles.js
+++ givantk-backend/controllers/profileController/getAllProfiles.js
@@ -6,6 +6,16 @@ const Profile = mongoose.model('profile');
 module.exports = getAllProfiles = (req, res) => {
   const errors = {};
   Profile.find()
+    .populate('services_asked_for')
+    .populate('services_helped_in')
+    .populate({
+      path: 'services_asked_for',
+      populate: { path: 'asker' }
+    })
+    .populate({
+      path: 'services_helped_in',
+      populate: { path: 'asker' }
+    })
     .sort({ date: -1 })
     .then((profiles) => {
       if (profiles.length === 0) {
