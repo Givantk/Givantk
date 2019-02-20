93f7951659a09217ab2f6ee61479e7754f03eff6
Osama Hafez <Eng.Osama.M.Hafez@gmail.com>
2019-02-20 19:06:21 +0200

    modify search method

--- givantk-backend/controllers/serviceController/search.js
+++ givantk-backend/controllers/serviceController/search.js
@@ -5,8 +5,15 @@ const Service = mongoose.model('service');
 
 module.exports = search = (req, res) => {
   const errors = {};
-  console.log(req.params.name);
-  Service.find({ name: req.params.name })
+  /* if the given search word is e.g.(wake) it will search for
+    - wake (case INSENSITIVE)
+    - wake + any number of words, like wake up call
+    - wake + any continuation of the word, like wakes..etc
+    
+    it will not search for a word in the middle e.g.(buy potato for me)
+    if you type "buy" it will find it but if you type "potato" it won't find it
+  */
+  Service.find({ name: { $regex: new RegExp("^" + req.params.name, "i") } })
     .sort({ date: -1 })
     .then((services) => {
       if (services.length === 0) {
