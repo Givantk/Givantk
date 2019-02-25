4ce77e0b12a67e1668ec9f8a4e910796574b7310
Mohamed Salah <mohamed2m2018@gmail.com>
2019-02-25 13:05:37 +0200

    replacing default profile image with another one

--- givantk-mobile/src/assets/data/fakeProfile.js
+++ givantk-mobile/src/assets/data/fakeProfile.js
@@ -4,7 +4,7 @@ const profile = {
   lastName: 'Moha',
   description:
     'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis ullam laborum voluptas, cum nam molestiae dolorem cupiditate odit, enim dicta maxime alias quibusdam explicabo. Nam ratione dignissimos ipsa repellendus maxime.',
-  avatar: 'https://avatars3.githubusercontent.com/u/5365516?s=88&v=4',
+  avatar: 'https://i.imgur.com/2FXmVPb.jpg',
   email: 'abdopoetry@gmail.com',
   dateOfBirth: new Date(),
   country: 'Egypt',
