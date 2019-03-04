13d4c2cea8bfa5d37ddb40fb47d71ae7cf01d7d9
Abdo <abdopoetry@gmail.com>
2019-03-04 18:10:26 +0200

    Fix last conflict

--- givantk-mobile/src/screens/commons/ChatScreen/ChatScreenStyles.js
+++ givantk-mobile/src/screens/commons/ChatScreen/ChatScreenStyles.js
@@ -6,17 +6,13 @@ const styles = StyleSheet.create({
   wrapper: {
     flex: 1,
     backgroundColor: colors.white,
-<<<<<<< HEAD
     alignItems: 'center',
     justifyContent: 'center',
-  },
-=======
-    marginTop: 24
+    marginTop: 24,
   },
   scrollView: {
-    marginBottom: 50
-  }
->>>>>>> f1bbb8a536b60b8db64c603800919635b34105d8
+    marginBottom: 50,
+  },
 });
 
 export default styles;
