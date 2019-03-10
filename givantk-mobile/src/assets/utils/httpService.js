78cdfe28615dab9afcb2c9c7ec7ac47075b01a06
Mohamed Salah <mohamed2m2018@gmail.com>
2019-03-10 15:08:36 +0200

    Connecting admin announcements to the backend

--- givantk-mobile/src/assets/utils/httpService.js
+++ givantk-mobile/src/assets/utils/httpService.js
@@ -22,9 +22,9 @@ export const removeAuthToken = () => {
   delete axios.defaults.headers.common.Authorization;
 };
 
-export const serverPath = 'http://192.168.43.91:5000/';
-export const userAPI = 'http://192.168.43.91:5000/api/user';
-export const serviceAPI = 'http://192.168.43.91:5000/api/service';
-export const profileAPI = 'http://192.168.43.91:5000/api/profile';
-export const paymentAPI = 'http://192.168.43.91:5000/api/payment';
-export const pointsAPI = 'http://192.168.43.91:5000/api/points';
+export const serverPath = 'http://192.168.0.8:5000/';
+export const userAPI = 'http://192.168.0.8:5000/api/user';
+export const serviceAPI = 'http://192.168.0.8:5000/api/service';
+export const profileAPI = 'http://192.168.0.8:5000/api/profile';
+export const paymentAPI = 'http://192.168.0.8:5000/api/payment';
+export const pointsAPI = 'http://192.168.0.8:5000/api/points';
