4b05f9bec691967fd2e18f19b12b89a8b2305e78
unknown <mohamed2m2018@gmail.com>
2019-04-24 20:10:30 +0200

    Editing icons

--- givantk-mobile/src/screens/commons/IntroScreen/IntroScreen.js
+++ givantk-mobile/src/screens/commons/IntroScreen/IntroScreen.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { StyleSheet, View, Text, Image } from 'react-native';
 import { LinearGradient } from 'expo';
 import AppIntroSlider from 'react-native-app-intro-slider';
 import { connect } from 'react-redux';
 import * as IntroActions from '../../../store/actions/introActions';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 
- 
 const styles = StyleSheet.create({
   mainContent: {
     flex: 1,
-
   },
   image: {
     width: 320,
@@ -28,75 +26,96 @@ const styles = StyleSheet.create({
     backgroundColor: 'transparent',
     textAlign: 'center',
     marginBottom: 16,
-  }
+  },
 });
- 
-const slides = [
+
+
+class IntroScreen extends React.Component {
+  
+ slides = [
   {
     key: 'Givantk',
     title: 'What is Givantk?',
-    text: 'Givantk is a services app, where you can ask for services or help others in services, the beta version of the app currently supports free services only ',
+    text:
+      'Givantk is a services app, where you can ask for services or help others in services, the beta version of the app currently supports free services only. ',
     icon: 'google',
     colors: ['#63E2FF', '#B066FE'],
   },
   {
-    key: 'GetStarted',
-    title: 'How to get started?',
-    text: 'Just create your profile, then you can view all the services on the app, and you can apply to help in any of them. If you want to ask for new service, it is very easy. Just click on request service tab   ',
-    icon: 'google',
+    key: 'Create profile',
+    title: 'Create your profile in few seconds',
+    text:
+      'Just create your profile in few seconds by adding your info and skills, then you can ask or apply for any service you want on the app.',
+    icon: 'account',
     colors: ['#A3A1FF', '#3A3897'],
   },
   {
     key: 'points',
-    title: 'What is Givantk Points?',
-    text: 'When you help others in their services you get points called Givantk Points. In future you can exchange this points with discounts or prizes from large stores.When you create your profile you will have 100 points, and you can use them to ask for services. ',
-    icon: 'google',
+    title: 'Get excited with Givantk Points',
+    text:
+      'When you create your profile, you get 100 Givantk Points, you can use them to ask for free services.To get more points you have to help others.\n \n Points can be exchanged in future with discounts in large stores. ',
+    icon: 'fire',
+    colors: ['#29ABE2', '#4F00BC'],
+  },
+  {
+    key: 'account-heart',
+    title: `We love you dear ${this.props.navigation.state.params.currentUser.first_name}`,
+    text:
+      'We love every member of our community, and we will always work hard to make sure our app is likable by you :D',
+    icon: 'account-heart',
     colors: ['#29ABE2', '#4F00BC'],
   },
 ];
- 
- class IntroScreen extends React.Component {
 
-
-    onDone=()=>{
-
-        const {navigation,passIntro}=this.props;
-        if (navigation.state.params) {
-
-            const { currentUser} = navigation.state.params;
-            //send to backend
-            passIntro(currentUser);
-        }
-
-        navigation.navigate('Featured');
-
-
+  onDone = () => {
+    const { navigation, passIntro } = this.props;
+    if (navigation.state.params) {
+      const { currentUser } = navigation.state.params;
+      //send to backend
+      passIntro(currentUser);
     }
 
-   
-  _renderItem = props => (
+    navigation.navigate('Featured');
+  };
+
+  _renderItem = (props) => (
     <LinearGradient
-      style={[styles.mainContent, {
-        paddingTop: props.topSpacer,
-        paddingBottom: props.bottomSpacer,
-        width: props.width,
-        height: props.height,
-      }]}
+      style={[
+        styles.mainContent,
+        {
+          paddingTop: props.topSpacer,
+          paddingBottom: props.bottomSpacer,
+          width: props.width,
+          height: props.height,
+        },
+      ]}
       colors={props.colors}
-      start={{x: 0, y: .1}} end={{x: .1, y: 1}}
+      start={{ x: 0, y: 0.1 }}
+      end={{ x: 0.1, y: 1 }}
     >
-      <MaterialCommunityIcons style={{ backgroundColor: 'transparent',alignSelf: 'center',marginTop:140, }} name={props.icon} size={200} color="white" />
-      <View style={{marginTop:10}}>
+    
+      <MaterialCommunityIcons
+        style={{
+          backgroundColor: 'transparent',
+          alignSelf: 'center',
+          marginTop: 120,
+        }}
+        name={props.icon}
+        size={200}
+        color="white"
+      />
+
+      <View style={{ marginTop: 10 }}>
         <Text style={styles.title}>{props.title}</Text>
         <Text style={styles.text}>{props.text}</Text>
       </View>
     </LinearGradient>
   );
- 
+
   render() {
     return (
       <AppIntroSlider
-        slides={slides}
+        slides={this.slides}
         renderItem={this._renderItem}
         bottomButton
         showSkipButton
@@ -106,14 +125,11 @@ const slides = [
   }
 }
 
+const mapDispatchToProps = {
+  passIntro: IntroActions.passIntro,
+};
 
-  
-  const mapDispatchToProps = {
-    passIntro: IntroActions.passIntro
-  };
-  
-  export default connect(
-    null,
-    mapDispatchToProps
-  )(IntroScreen);
-  
+export default connect(
+  null,
+  mapDispatchToProps
+)(IntroScreen);
