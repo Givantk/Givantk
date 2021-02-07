619d1aad41bc04e28f7886cec61d4084da15262e
Mohamed Mohamed Salah <mohamed2m2018@gmail.com>
2021-02-08 00:13:23 +0200

    Excluding user from appearing to himself in recommended helpers

--- givantk-backend/controllers/serviceController/getRecommendedHelpers.js
+++ givantk-backend/controllers/serviceController/getRecommendedHelpers.js
@@ -1,67 +1,70 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 //Get Model
 
-const serviceModel = mongoose.model('service');
+const serviceModel = mongoose.model('service')
 
-const profileModel = mongoose.model('profile');
+const profileModel = mongoose.model('profile')
 
 const CalculateScore = (profile, service) => {
+  if (service.asker._id.toString() === profile.user._id.toString()) {
+    profile.score = 0
+    return
+  }
+
   let {
     skills: recommendedSkills,
     job: recommendedJobs,
-    location: recommendedLocations
-  } = service.recommenderInfo;
+    location: recommendedLocations,
+  } = service.recommenderInfo
   let {
     skills: profileSkills,
     job: profileJob,
-    location: profileLocation
-  } = profile.recommenderInfo;
+    location: profileLocation,
+  } = profile.recommenderInfo
 
   //Match common skills and assign score
-  matchedSkills = recommendedSkills.filter((skill) =>
-    profileSkills.includes(skill)
-  );
+  matchedSkills = recommendedSkills.filter(skill => profileSkills.includes(skill))
 
-  profile.matchedSkills = matchedSkills;
-  profile.score = matchedSkills.length;
+  profile.matchedSkills = matchedSkills
+  profile.score = matchedSkills.length
 
   //Match common job and assign score
 
   for (job of recommendedJobs) {
     if (job === profileJob) {
-      profile.score += 1;
-      break;
+      profile.score += 1
+      break
     }
   }
 
   //Match common location and assign score
   for (location of recommendedLocations) {
     if (location === profileLocation) {
-      profile.score += 1;
-      break;
+      profile.score += 1
+      break
     }
   }
-};
+}
 
 module.exports = getRecommendedHelpers = (req, res) => {
-  const errors = {};
-  const serviceId = req.params.service_id;
-  serviceModel.findById(serviceId).then((service) => {
+  const errors = {}
+  const serviceId = req.params.service_id
+  serviceModel.findById(serviceId).then(service => {
     profileModel
       .find()
-      .then((profiles) => {
+      .then(profiles => {
         if (profiles.length === 0) {
-          errors.noProfiles = 'No profiles Found';
+          errors.noProfiles = 'No profiles Found'
         }
-        profiles.forEach((profile) => CalculateScore(profile, service));
-        profiles.sort((a, b) => b.score - a.score);
+        profiles.forEach(profile => CalculateScore(profile, service))
+        profiles.sort((a, b) => b.score - a.score)
         //send the response containing profiles sorted by recommendation scores for this service
-        res.json(profiles);
+        res.json(profiles)
       })
-      .catch((err) => {
-        errors.error = 'Error getting recommended users';
-        res.status(500).json({ ...errors, ...err });
-      });
-  });
-};
+      .catch(err => {
+        errors.error = 'Error getting recommended users'
+        res.status(500).json({ ...errors, ...err })
+      })
+  })
+}
