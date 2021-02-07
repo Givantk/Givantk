07523bbdb7c40e0e90237036016acda27be47aee
Mohamed Mohamed Salah <mohamed2m2018@gmail.com>
2021-02-08 00:26:11 +0200

    Disable warning temporarily

--- givantk-mobile/App.js
+++ givantk-mobile/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 import AppConfigured from './src/AppConfigured';
 
+// eslint-disable-next-line no-console
+console.disableYellowBox = true;
+
 const App = () => <AppConfigured />;
 
 export default App;
